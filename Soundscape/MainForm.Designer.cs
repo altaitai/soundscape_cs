60001b392de0316122c3b4be656bd60216ad31d1
Alexandre Taillefer <alexandre.taillefer0@gmail.com>
2020-11-10 22:09:59 -0500

    Added copy, paste, drag-drop out and fixed minor bugs

--- Soundscape/MainForm.Designer.cs
+++ Soundscape/MainForm.Designer.cs
@@ -35,12 +35,16 @@
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.MainStatusStrip = new System.Windows.Forms.StatusStrip();
-            this.SoundView = new System.Windows.Forms.ListView();
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.libraryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainStatusStrip = new System.Windows.Forms.StatusStrip();
             this.OpenFileStatus = new System.Windows.Forms.ToolStripStatusLabel();
             this.SoundCountStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.SoundView = new System.Windows.Forms.ListView();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.soundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MainFormMenuStrip.SuspendLayout();
             this.MainStatusStrip.SuspendLayout();
             this.SuspendLayout();
@@ -54,7 +58,7 @@
             this.editToolStripMenuItem});
             this.MainFormMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MainFormMenuStrip.Name = "MainFormMenuStrip";
-            this.MainFormMenuStrip.Size = new System.Drawing.Size(1504, 48);
+            this.MainFormMenuStrip.Size = new System.Drawing.Size(1504, 40);
             this.MainFormMenuStrip.TabIndex = 0;
             this.MainFormMenuStrip.Text = "menuStrip1";
             // 
@@ -73,37 +77,62 @@
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(377, 44);
             this.newToolStripMenuItem.Text = "New";
             this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(377, 44);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(377, 44);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(377, 44);
             this.saveAsToolStripMenuItem.Text = "Save As";
             this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // quitToolStripMenuItem
             // 
             this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
-            this.quitToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(377, 44);
             this.quitToolStripMenuItem.Text = "Quit";
             this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.libraryToolStripMenuItem,
+            this.soundToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.removeToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(75, 38);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // libraryToolStripMenuItem
+            // 
+            this.libraryToolStripMenuItem.Name = "libraryToolStripMenuItem";
+            this.libraryToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.libraryToolStripMenuItem.Text = "Library";
+            this.libraryToolStripMenuItem.Click += new System.EventHandler(this.libraryToolStripMenuItem_Click);
             // 
             // MainStatusStrip
             // 
@@ -117,39 +146,6 @@
             this.MainStatusStrip.TabIndex = 1;
             this.MainStatusStrip.Text = "MainStatusStrip";
             // 
-            // SoundView
-            // 
-            this.SoundView.Activation = System.Windows.Forms.ItemActivation.OneClick;
-            this.SoundView.AllowColumnReorder = true;
-            this.SoundView.AllowDrop = true;
-            this.SoundView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.SoundView.FullRowSelect = true;
-            this.SoundView.HideSelection = false;
-            this.SoundView.Location = new System.Drawing.Point(0, 48);
-            this.SoundView.Name = "SoundView";
-            this.SoundView.Size = new System.Drawing.Size(1504, 792);
-            this.SoundView.Sorting = System.Windows.Forms.SortOrder.Ascending;
-            this.SoundView.TabIndex = 2;
-            this.SoundView.UseCompatibleStateImageBehavior = false;
-            this.SoundView.ItemActivate += new System.EventHandler(this.SoundView_ItemActivate);
-            this.SoundView.DragDrop += new System.Windows.Forms.DragEventHandler(this.SoundView_DragDrop);
-            this.SoundView.DragEnter += new System.Windows.Forms.DragEventHandler(this.SoundView_DragEnter);
-            // 
-            // editToolStripMenuItem
-            // 
-            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.libraryToolStripMenuItem});
-            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(75, 36);
-            this.editToolStripMenuItem.Text = "Edit";
-            // 
-            // libraryToolStripMenuItem
-            // 
-            this.libraryToolStripMenuItem.Name = "libraryToolStripMenuItem";
-            this.libraryToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
-            this.libraryToolStripMenuItem.Text = "Library";
-            this.libraryToolStripMenuItem.Click += new System.EventHandler(this.libraryToolStripMenuItem_Click);
-            // 
             // OpenFileStatus
             // 
             this.OpenFileStatus.Name = "OpenFileStatus";
@@ -159,6 +155,55 @@
             // 
             this.SoundCountStatus.Name = "SoundCountStatus";
             this.SoundCountStatus.Size = new System.Drawing.Size(0, 12);
+            // 
+            // SoundView
+            // 
+            this.SoundView.AllowColumnReorder = true;
+            this.SoundView.AllowDrop = true;
+            this.SoundView.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.SoundView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SoundView.FullRowSelect = true;
+            this.SoundView.HideSelection = false;
+            this.SoundView.Location = new System.Drawing.Point(0, 40);
+            this.SoundView.Name = "SoundView";
+            this.SoundView.Size = new System.Drawing.Size(1504, 800);
+            this.SoundView.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.SoundView.TabIndex = 2;
+            this.SoundView.UseCompatibleStateImageBehavior = false;
+            this.SoundView.ItemActivate += new System.EventHandler(this.SoundView_ItemActivate);
+            this.SoundView.DragDrop += new System.Windows.Forms.DragEventHandler(this.SoundView_DragDrop);
+            this.SoundView.DragEnter += new System.Windows.Forms.DragEventHandler(this.SoundView_DragEnter);
+            this.SoundView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SoundView_MouseDown);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // soundToolStripMenuItem
+            // 
+            this.soundToolStripMenuItem.Name = "soundToolStripMenuItem";
+            this.soundToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.soundToolStripMenuItem.Text = "Sound";
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.Delete;
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.removeToolStripMenuItem_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
             // 
             // MainForm
             // 
@@ -197,6 +242,10 @@
         private System.Windows.Forms.ToolStripMenuItem libraryToolStripMenuItem;
         private System.Windows.Forms.ToolStripStatusLabel OpenFileStatus;
         private System.Windows.Forms.ToolStripStatusLabel SoundCountStatus;
+        private System.Windows.Forms.ToolStripMenuItem soundToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
     }
 }
 
